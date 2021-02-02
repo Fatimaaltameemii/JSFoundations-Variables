5dc358655add6eb24f59644f3f81ec59070121df
Fatimaaltameemii <78157712+Fatimaaltameemii@users.noreply.github.com>
2021-02-02 16:29:20 +0300

    Task 3
    i did it :+1:

---
+++ story.js
@@ -0,0 +1,9 @@
+let name = "Fatima";
+let age = 22;
+let hobby = "Diving";
+
+console.log(
+  `Mr./Mrs. ${name} , in 33 years, you will be ${
+    age + 33
+  }  years old. By then youll have decades of ${hobby} practice`
+);
